e63517defb74c5f3b2ee07db15a49780ff315a16
Lakshay Nirwal <lakshaynirwal26@gmail.com>
2025-08-16 17:50:17 +0530

    changes done

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Login from '../components/Login';
-import Signup from '../components/signup';
+import Signup from '../components/Signup';
 import { UrlState } from '@/Context';
 
 const Auth = () => {
